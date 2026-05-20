ef7619c12d1ac806a08b61a2ef8b35396724268b
javad-rzvn <javad.rezvanpour@gmail.com>
2026-05-20 14:56:41 +0330

    Change: modify config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,15 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+  const isUserOrOrgPagesRepo = repoName?.toLowerCase().endsWith('.github.io');
+  const base =
+    process.env.GITHUB_ACTIONS === 'true' && repoName && !isUserOrOrgPagesRepo
+      ? `/${repoName}/`
+      : '/';
+
   return {
+    base,
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
